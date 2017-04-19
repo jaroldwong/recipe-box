6c2eb7301eb871c4e74f23819954e09dc0c231af
Jarold Wong <jaroldw@gmail.com>
2017-04-19 12:10:30 -0700

    Display recipe cards

--- src/app.js
+++ src/app.js
@@ -4,13 +4,39 @@ var ReactDOM = require('react-dom');
 require('./style.css');
 
 var Recipes = React.createClass({
+    getInitialState: function() {
+        return {
+                recipes: [
+                    {name: "grilled cheese", ingredients: ["bread", "american cheese"]},
+                    {name: "quesadilla", ingredients: ["tortilla", "cheese"]}
+                ]
+        };
+    }, // getInitialState
     render: function() {
         return(
             <div>
-                Recipe Box
+                <h1>Recipe Box</h1>
+                <RecipeCard recipes={this.state.recipes} />
             </div>
         );
-    }
+    }, // render
 });
+
+var RecipeCard = React.createClass({
+    render: function() {
+        return(
+            <div>
+                {this.props.recipes.map(function(recipe, index){
+                    return(
+                        <div className="card" key={index}>
+                            <h3>{recipe.name.toUpperCase()}</h3>
+                            <p>{recipe.ingredients.join(', ')}</p>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }, // render
+})
 
 ReactDOM.render(<Recipes />, document.getElementById('root'));
